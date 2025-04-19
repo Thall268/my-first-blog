85b27315bdadf78e9e7a8284bc83cc7cf8ecf73b
Sparky2Uman <thall268@ivytech.edu>
2025-04-19 04:29:39 -0500

    Fix: properly separate views and URL patterns

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,3 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
-from django.shortcuts import render
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
